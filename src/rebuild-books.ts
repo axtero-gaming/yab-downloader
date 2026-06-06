d019155f6cee321b9922bfc8b0bb42848a3d47cd
Andrey Shuleiko <geass1000@gmail.com>
2026-06-06 14:30:47 +0300

    feat(app): rename scripts

--- src/rebuild-books.ts
+++ src/rebuild-books.ts
@@ -2,13 +2,15 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { BookInfo, BookPage } from './shared/types';
 import { buildBookFolderPath } from './utils/book.utils';
-import { buildFB2BookFile } from './book-file-builder';
+import { buildEpubBookFile, buildFB2BookFile, buildHTMLBookFile } from './book-file-builder';
 
-const bookId = `V2Lfcler`;
+const bookId = `dcnbjwFA`;
 const bookPath = buildBookFolderPath(bookId);
 const pagesFilePath = path.resolve(bookPath, 'pages.json');
 const pages: BookPage[] = JSON.parse(await fs.readFile(pagesFilePath, 'utf8'));
 const infoFilePath = path.resolve(bookPath, 'info.json');
 const bookInfo: BookInfo = JSON.parse(await fs.readFile(infoFilePath, 'utf8'));
 
+await buildHTMLBookFile(bookId, pages);
+await buildEpubBookFile(bookId, pages, bookInfo);
 await buildFB2BookFile(bookId, pages, bookInfo);
